889e62fdbffde224d84c7f6a0314cb4577fca81c
arunser <stormbreaker.004@gmail.com>
2023-12-06 18:30:12 +0530

    ros2 camera calibration  package

---
+++ luminosity_drone/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import os 
+from glob import glob
+
+package_name = 'luminosity_drone'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='arunp',
+    maintainer_email='stormbreaker.004@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
